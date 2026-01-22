7ffc16c7a0ef991862368f65132333a6017f4dbd
Elizabeth Shkinder <elizabethshkinder@elizabeths-mbp.home>
2026-01-22 16:54:54 +0000

    install modern-normalize and @tanstack/react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'modern-normalize/modern-normalize.css';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import App from './components/App/App'
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
+
+
